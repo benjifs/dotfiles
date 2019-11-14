17f82047e3f46aa0a54147242d3fff6001474415
Benji Encalada Mora <benjifs@users.noreply.github.com>
2019-11-14 16:35:19 -0600

    changed the way zcompdump is generated

--- config/zsh/completion.zsh
+++ config/zsh/completion.zsh
@@ -4,11 +4,15 @@ autoload -Uz compinit
 
 # Speed up zsh compinit
 # https://carlosbecker.com/posts/speeding-up-zsh
-if [ $(date +'%j') != $(stat -f '%Sm' -t '%j' ~/.zcompdump) ]; then
-	compinit
+# Switched to prezto's implementation of zcompdump to work everywhere
+# https://github.com/sorin-ionescu/prezto
+_comp_files=(${ZDOTDIR:-$HOME}/.zcompdump(Nm-20))
+if (( $#_comp_files )); then
+	compinit -i -C
 else
-	compinit -C
+	compinit -i
 fi
+unset _comp_files
 
 zstyle ':completion:*' menu select
 zstyle ':completion:*' completer _complete _approximate _expand
@@ -16,3 +20,8 @@ zstyle ':completion:*:default' list-colors "${(@s.:.)LS_COLORS}"
 # zstyle ':completion:*' matcher-list '' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' '+l:|=* r:|=*'
 
 setopt menu_complete
+# Testing
+setopt auto_list # automatically list choices on ambiguous completion
+setopt auto_menu # automatically use menu completion
+setopt always_to_end # move cursor to end if word had one match
+
